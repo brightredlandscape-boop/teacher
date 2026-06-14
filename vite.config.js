53787e1e74e234f3e5c172f5269fc5809385425a
zeerocodez <edeteffiong20002@gmail.com>
2026-06-14 17:46:22 +0100

    fix: adjust Vite base path dynamically for Vercel deployment to resolve blank page assets crash

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/EduBridge/' : '/',
+  base: (process.env.NODE_ENV === 'production' && !process.env.VERCEL) ? '/EduBridge/' : '/',
   plugins: [react()],
   server: {
     allowedHosts: ['.loca.lt', '.ngrok-free.app', '.ngrok-free.dev', 'localhost'],
